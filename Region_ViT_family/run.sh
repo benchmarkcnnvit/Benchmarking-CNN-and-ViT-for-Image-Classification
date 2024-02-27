bed2903742a12c0d8a7c6201ef8b2ae5826a6830
benchmarkcnnvit <158844533+benchmarkcnnvit@users.noreply.github.com>
2024-02-27 12:15:28 -0600

    Delete Region_ViT_family directory

--- Region_ViT_family/run.sh
+++
@@ -1,8 +0,0 @@
-python RegionViT_base.py
-python RegionViT_base_w14.py
-python RegionViT_base_w14_peg.py
-python RegionViT_medium.py
-python RegionViT_small.py
-python RegionViT_small_w14.py
-python RegionViT_small_w14_peg.py
-python RegionViT_tiny.py
